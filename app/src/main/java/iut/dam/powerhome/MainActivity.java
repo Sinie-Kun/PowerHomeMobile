c4224dd11388a16301047b4e3106566cd8f5c31f
Adam Ziane <adam.raphael.ziane@gmail.com>
2025-03-30 16:02:51 +0200

    petit commit

--- app/src/main/java/iut/dam/powerhome/MainActivity.java
+++ app/src/main/java/iut/dam/powerhome/MainActivity.java
@@ -17,87 +17,84 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navView = findViewById(R.id.nav_view);
+
+        // Ajouter l’icône du menu (≡)
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true); // (optionnel mais souvent utile)
+        }
+
+        // Afficher le fragment par défaut au lancement
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new HabitatsFragment())
+                    .commit();
+            navView.setCheckedItem(R.id.nav_habitats);
+        }
+
+        // Navigation entre les fragments
+        navView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_habitats){
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new HabitatsFragment())
+                        .commit();
+            }
+            else if (id == R.id.nav_myhabitat){
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new MyHabitatFragment())
+                        .commit();
+            }
+            else if (id == R.id.nav_mynotifications){
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new MyNotificationsFragment())
+                        .commit();
+            }
+            else if (id == R.id.nav_mypreferences){
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new MyPreferencesFragment())
+                        .commit();
+            }
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
     }
-}
-//        setContentView(R.layout.activity_main);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        drawerLayout = findViewById(R.id.drawer_layout);
-//        navView = findViewById(R.id.nav_view);
-//
-//        // Ajouter l’icône du menu (≡)
-//        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer);
-//        drawerLayout.addDrawerListener(toggle);
-//        toggle.syncState();
-//
-//        if (getSupportActionBar() != null) {
-//            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//            getSupportActionBar().setHomeButtonEnabled(true); // (optionnel mais souvent utile)
-//        }
-//
-//        // Afficher le fragment par défaut au lancement
-//        if (savedInstanceState == null) {
-//            getSupportFragmentManager().beginTransaction()
-//                    .replace(R.id.fragment_container, new HabitatsFragment())
-//                    .commit();
-//            navView.setCheckedItem(R.id.nav_habitats);
-//        }
-//
-//        // Navigation entre les fragments
-//        navView.setNavigationItemSelectedListener(item -> {
-//            int id = item.getItemId();
-//            if (id == R.id.nav_habitats){
-//                getSupportFragmentManager().beginTransaction()
-//                        .replace(R.id.fragment_container, new HabitatsFragment())
-//                        .commit();
-//            }
-//            else if (id == R.id.nav_myhabitat){
-//                getSupportFragmentManager().beginTransaction()
-//                        .replace(R.id.fragment_container, new MyHabitatFragment())
-//                        .commit();
-//            }
-//            else if (id == R.id.nav_mynotifications){
-//                getSupportFragmentManager().beginTransaction()
-//                        .replace(R.id.fragment_container, new MyNotificationsFragment())
-//                        .commit();
-//            }
-//            else if (id == R.id.nav_mypreferences){
-//                getSupportFragmentManager().beginTransaction()
-//                        .replace(R.id.fragment_container, new MyPreferencesFragment())
-//                        .commit();
-//            }
-//
-//            drawerLayout.closeDrawer(GravityCompat.START);
-//            return true;
-//        });
-//    }
-//
-//    // Synchroniser l'icône du menu avec l’ouverture du drawer
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        return toggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
-//    }
-//
-//
-//
-//        String urlString = "http:// [IP] /powerhome_server/getHabitats.php";
-//    }
-//    /*public void getRemoteHabitats() {
-//        String urlString = "http://[server]/powerhome_server/getHabitats.php";
-//        Ion.with(this)
-//                .load(urlString)
-//                .asString()
-//                .setCallback(new FutureCallback<String>() {
-//                    @Override
-//                    public void onCompleted(Exception e, String result) {
-//                        pDialog.dismiss();
-//                        if(result == null)
-//                            Log.d(TAG, "No response from the server!!!");
-//                        else {
-//                        }
-//                    }
-//                    // Traitement de result
-//                }
-//    });*/
+
+    // Synchroniser l'icône du menu avec l’ouverture du drawer
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return toggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+
+
+        String urlString = "http:// [IP] /powerhome_server/getHabitats.php";
+    }
+    /*public void getRemoteHabitats() {
+        String urlString = "http://[server]/powerhome_server/getHabitats.php";
+        Ion.with(this)
+                .load(urlString)
+                .asString()
+                .setCallback(new FutureCallback<String>() {
+                    @Override
+                    public void onCompleted(Exception e, String result) {
+                        pDialog.dismiss();
+                        if(result == null)
+                            Log.d(TAG, "No response from the server!!!");
+                        else {
+                        }
+                    }
+                    // Traitement de result
+                }
+    });*/
