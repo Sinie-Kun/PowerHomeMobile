ff4f804d1de9c7c73a95f0f8fb567d588c48abbd
Jojo <jorys.kancel@gmail.com>
2025-03-31 22:58:10 +0200

    splash

--- app/src/main/java/iut/dam/powerhome/SplashActivity.java
+++ app/src/main/java/iut/dam/powerhome/SplashActivity.java
@@ -1,24 +1,42 @@
 package iut.dam.powerhome;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import iut.dam.powerhome.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ImageView logoImage;
+    private static final int SPLASH_DURATION = 5000; // 5 secondes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Récupère le logo
+        logoImage = findViewById(R.id.logoImage);
+
+        // Lance l’animation de rotation
+        Animation rotate = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        logoImage.startAnimation(rotate);
+
+        // Redirection après 5 secondes
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Démarre LoginActivity
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // Ferme SplashActivity pour ne pas revenir dessus avec le bouton "back"
+            }
+        }, SPLASH_DURATION);
     }
 }
